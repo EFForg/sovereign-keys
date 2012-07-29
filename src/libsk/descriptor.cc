fea345e7c6cc526c057fe65bc5a8b56c4644b9e8
Jered Wierzbicki <jeredw@gmail.com>
2012-07-29 11:32:12 -0700

    Remove unreferenced methods.

--- src/libsk/descriptor.cc
+++ src/libsk/descriptor.cc
@@ -2,15 +2,6 @@
 
 #include "descriptor.h"
 
-#include "add_root_ca_descriptor.h"
-#include "bind_descriptor.h"
-#include "change_services_descriptor.h"
-#include "rebind_descriptor.h"
-#include "remove_root_ca_descriptor.h"
-#include "slice.h"
-#include "tfm_descriptor.h"
-#include "unbind_descriptor.h"
-
 namespace sk {
 
 Descriptor::Descriptor(int version)
--- src/libsk/descriptor.h
+++ src/libsk/descriptor.h
@@ -10,17 +10,10 @@ namespace sk {
 class Field;
 class Slice;
 
-// Interface for classes which expose details of each log entry type
-// such as its name and fields.
+// Interface for querying the field layout in a message type.
 class Descriptor {
  public:
   virtual ~Descriptor();
-
-  // Factory methods to create descriptors from a message type name or id.
-  // These return a new derived class instance or NULL if the message type
-  // is unknown.
-  static const Descriptor* GetByName(Slice name, int version);
-  static const Descriptor* GetByType(int entry_type, int version);
 
   // Returns the type name for canonical form serialization.
   virtual const char* GetTypeName() const = 0;
@@ -45,7 +38,7 @@ class Descriptor {
  private:
   DISALLOW_EVIL_CONSTRUCTORS(Descriptor);
 
-  // Which version of the log entry this descriptor is for.
+  // Which version of the message this descriptor is for.
   int version_;
 };
 
